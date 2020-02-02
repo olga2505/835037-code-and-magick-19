fac47daac214e8df8743853b5bfb6b46591e331c
Olga <151590@mail.ru>
2020-02-02 12:56:12 +0300

    Создание персонажей (с исправлениями 2)

--- js/setup.js
+++ js/setup.js
@@ -1,36 +1,35 @@
 'use strict';
 
-var getRemoveClass = function () {
-  document.querySelector('.setup').classList.remove('hidden');
-  document.querySelector('.setup-similar').classList.remove('hidden');
-};
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
 
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var showPopup = function () {
+  document.querySelector('.setup').classList.remove('hidden');
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+
 var getRandomElement = function (items) {
   return items[Math.floor(Math.random() * items.length)];
 };
 
-var createWizardsArray = function (len) {
-  for (var i = 1, arr = []; i <= len; i++) {
-    arr.push({
+var createWizardsArray = function (length) {
+  for (var i = 1, array = []; i <= length; i++) {
+    array.push({
       'name': getRandomElement(FIRST_NAMES) + ' ' + getRandomElement(LAST_NAMES),
       'coatColor': getRandomElement(COAT_COLORS),
       'eyesColor': getRandomElement(EYES_COLORS)
     });
   }
-  return arr;
+  return array;
 };
-var wizardsArray = createWizardsArray(4);
 
 var renderWizard = function (wizard) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -49,6 +48,8 @@ var renderWizards = function (wizards) {
   var similarListElement = document.querySelector('.setup-similar-list');
   similarListElement.appendChild(fragment);
 };
-renderWizards(wizardsArray);
+var wizardsAll = createWizardsArray(4);
 
-getRemoveClass();
+renderWizards(wizardsAll);
+
+showPopup();
