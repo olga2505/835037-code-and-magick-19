93c16ee709cc8b306b2a2b514aa50e698a414b94
Olga <151590@mail.ru>
2020-02-29 17:06:52 +0300

    Возврвщение окна на место

--- js/moving.js
+++ js/moving.js
@@ -3,6 +3,8 @@
 (function () {
   var setup = document.querySelector('.setup');
   var dialogHandler = setup.querySelector('.upload');
+  var setupClose = document.querySelector('.setup-close');
+
 
   dialogHandler.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -14,10 +16,9 @@
 
     var dragged = false;
 
+
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-      dragged = true;
-
       var shift = {
         x: startCoords.x - moveEvt.clientX,
         y: startCoords.y - moveEvt.clientY
@@ -30,7 +31,16 @@
 
       setup.style.top = (setup.offsetTop - shift.y) + 'px';
       setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+
+      if (shift.x !== 0 || shift.y !== 0) {
+        dragged = true;
+      }
     };
+
+    setupClose.addEventListener('click', function () {
+      setup.style.top = '';
+      setup.style.left = '';
+    });
 
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
@@ -48,7 +58,6 @@
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
-
   });
 })();
 
